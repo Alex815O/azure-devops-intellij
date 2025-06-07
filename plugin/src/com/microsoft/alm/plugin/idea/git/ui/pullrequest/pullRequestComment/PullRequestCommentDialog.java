711d199e0000cd1ce90b466a212a0a37f982566d
Alex Ofner <e12119038@student.tuwien.ac.at>
2025-06-07 23:28:14 +0200

    bugfix: increase UI

--- plugin/src/com/microsoft/alm/plugin/idea/git/ui/pullrequest/pullRequestComment/PullRequestCommentDialog.java
+++ plugin/src/com/microsoft/alm/plugin/idea/git/ui/pullrequest/pullRequestComment/PullRequestCommentDialog.java
@@ -1,8 +1,12 @@
 package com.microsoft.alm.plugin.idea.git.ui.pullrequest.pullRequestComment;
 
+import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBScrollPane;
+import com.intellij.util.ui.JBUI;
 import com.microsoft.alm.plugin.external.models.pullRequestThread.Comment;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -10,11 +14,15 @@ import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
+import javax.swing.UIManager;
+import javax.swing.border.MatteBorder;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -34,15 +42,19 @@ public class PullRequestCommentDialog extends JDialog {
         this.controller = controller;
 
         setTitle("Findings");
-        contentPane.setPreferredSize(new Dimension(550, 400));
-        JBScrollPane scrollPane = new JBScrollPane(contentPane);
 
+        contentPane.setBorder(JBUI.Borders.empty(10));
+        contentPane.setAutoscrolls(true);
+        JBScrollPane scrollPane = new JBScrollPane(contentPane);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        setMinimumSize(new Dimension(550, 100));
+        pack();
         setContentPane(scrollPane);
+
+
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
-
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -86,33 +98,37 @@ public class PullRequestCommentDialog extends JDialog {
     }
 
     private JPanel createCommentTextField(Comment comment) {
-        JPanel commentPanel = new JPanel(new GridBagLayout());
+        JPanel commentPanel = new JPanel();
+        commentPanel.setLayout(new BoxLayout(commentPanel, BoxLayout.Y_AXIS));
+        commentPanel.setBorder(BorderFactory.createEmptyBorder(5, 2, 2, 2));
 
-        GridBagConstraints left = new GridBagConstraints();
-        left.gridx = 0;
-        left.gridy = 0;
-        left.weightx = 0.1;
-        left.weighty = 1.0;
-        left.fill = GridBagConstraints.BOTH;
+        // Autor-Label (oben links)
+        var authorLabel = new JLabel(comment.getAuthor().getDisplayName() + ":");
+        authorLabel.setFont(UIManager.getFont("Label.font").deriveFont(Font.BOLD, 14));
+        authorLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+        commentPanel.add(authorLabel);
 
-        GridBagConstraints right = new GridBagConstraints();
-        right.gridx = 1;
-        right.gridy = 0;
-        right.weightx = 0.9;
-        right.weighty = 1.0;
-        right.fill = GridBagConstraints.BOTH;
+        // Abstand zwischen Label und TextArea
+        commentPanel.add(Box.createRigidArea(new Dimension(0, 5)));
 
+        // TextArea mit Scroll (nutzt verfügbaren Platz)
+        var textArea = new JTextArea();
+        textArea.setText(comment.getContent());
+        textArea.setEditable(false);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setFont(UIManager.getFont("Label.font"));
+        textArea.setOpaque(false);
+        textArea.setBackground(new Color(0,0,0,0));
 
-        var textField = new JTextField();
-        var authorLabel = new JLabel(comment.getAuthor().getDisplayName());
+        JScrollPane scrollPane = new JBScrollPane(textArea);
+        scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        scrollPane.setBorder(null);
+        scrollPane.setViewportBorder(null);
+        scrollPane.setOpaque(false);
+        scrollPane.getViewport().setBackground(new Color(0,0,0,0));
 
-        textField.setText(comment.getContent());
-        textField.setEditable(false);
-        textField.setOpaque(false);
-        textField.setFocusable(false);
-
-        commentPanel.add(authorLabel, left);
-        commentPanel.add(textField, right);
+        commentPanel.add(scrollPane);
 
         return commentPanel;
     }
@@ -129,6 +145,7 @@ public class PullRequestCommentDialog extends JDialog {
 
         commentPanel = new JPanel();
         commentPanel.setLayout(new BoxLayout(commentPanel, BoxLayout.Y_AXIS));
+        commentPanel.setBorder(new MatteBorder(0, 0, 1, 0, JBColor.BLACK));
 
         int idx = 0;
         for (Comment comment : comments) {
